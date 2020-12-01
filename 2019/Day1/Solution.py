833347bd1e9187f10bd19eda805695a141b76a1e
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2020-12-01 22:08:30 +0100

    some work on 2019

--- 2019/Day1/Solution.py
+++ 2019/Day1/Solution.py
@@ -1,7 +1,5 @@
 ﻿import sys
 import os
-import math
-
 
 dir_path = sys.path[0]
 file_path = os.path.join(dir_path, "input.txt")
@@ -10,8 +8,30 @@ list_of_module_masses = []
 
 with open(file_path, "r") as input_file:
     for line in input_file:
-        list_of_module_masses.append(int(line.strip()))
+        list_of_module_masses.append(int(line))
 
-total_needed_fuel = sum(
-    [math.floor(module_mass/3)-2 for module_mass in module_masses])
-print(total_needed_fuel)
+
+def calc_fuel(mass):
+    fuel_needed = mass // 3 - 2
+    if fuel_needed < 0:
+        return 0
+    else:
+        return fuel_needed
+
+
+# Part 1
+part_1_fuel = sum([calc_fuel(module_mass) for module_mass in list_of_module_masses])
+print(part_1_fuel)
+
+
+# Part 2
+def calc_fuel_recursive(mass):
+    fuel_needed = mass // 3 - 2
+    if fuel_needed <= 0:
+        return 0
+    else:
+        return fuel_needed + calc_fuel_recursive(fuel_needed)
+
+
+total_fuel_needed_recursive = sum([calc_fuel_recursive(element) for element in list_of_module_masses])
+print(total_fuel_needed_recursive)
---
+++ 2019/Day2/Solution.py
@@ -0,0 +1,53 @@
+﻿import sys
+import os
+
+dir_path = sys.path[0]
+file_path = os.path.join(dir_path, "input.txt")
+
+spaceship_intcode = []
+
+with open(file_path, "r") as input_file:
+    for entry in input_file:
+        for num in entry.split(","):
+            spaceship_intcode.append(int(num))
+
+# preparation
+spaceship_intcode[1] = 12
+spaceship_intcode[2] = 2
+
+test_intcode = [1, 0, 0, 3, 99]
+# modifies input object
+def intcode_computer(intcode):
+    output = [x for x in intcode]
+    skip = 0
+    for idx, cmd in enumerate(intcode):
+        if skip > 0:
+            print("skipping")
+            skip -= 1
+        else:
+            print(f"current command: {cmd}")
+            if cmd == 99:
+                break
+            elif cmd == 1:
+                value_1 = intcode[intcode[idx + 1]]
+                value_2 = intcode[intcode[idx + 2]]
+                output_idx = intcode[intcode[idx + 3]]
+                result = value_1 + value_2
+                print(result)
+                output[output_idx] = result
+                print(output[output_idx])
+                skip = 3
+            elif cmd == 2:
+                value_1 = intcode[intcode[idx + 1]]
+                value_2 = intcode[intcode[idx + 2]]
+                output_idx = intcode[intcode[idx + 3]]
+                result = value_1 * value_2
+                output[output_idx] = result
+                skip = 3
+            else:
+                pass
+    return output
+
+
+res = intcode_computer(test_intcode)
+print(res)
