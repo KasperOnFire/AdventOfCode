562f0708a84fc39f488fc4cd6a1dad3d4f510aca
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2021-12-02 16:42:07 +0100

    day 2 done

--- 2021/Solutions/Day1.py
+++ 2021/Solutions/Day1.py
@@ -8,23 +8,27 @@ with open(input_file) as inputs:
     for i in inputs:
         sonar_readings.append(int(i))
 
-def part_1(sonar_readings: list) -> int:
+
+def part_1(input_readings: list) -> int:
     incr = 0
-    for i, reading in enumerate(sonar_readings):
-        if reading > sonar_readings[i-1]:
+    for i, reading in enumerate(input_readings):
+        if reading > input_readings[i - 1]:
             incr += 1
     return incr
 
+
 print(part_1(sonar_readings))
 
-def part_2(sonar_readings: list) -> int:
+
+def part_2(input_readings: list) -> int:
     incr = 0
     last = 0
-    for i in range(len(sonar_readings)-2):
-        window = sum(sonar_readings[i:i+3])
+    for i in range(len(input_readings) - 2):
+        window = sum(input_readings[i:i + 3])
         if i > 0 and window > last:
             incr += 1
         last = window
     return incr
+
 
 print(part_2(sonar_readings))
--- 2021/Solutions/Day2.py
+++ 2021/Solutions/Day2.py
@@ -1,7 +1,10 @@
-def calculate_route_and_return_product(data: List[(str, int)]) -> int:
+from pathlib import Path
+
+
+def calculate_route_and_return_product(inputs: list[(str, int)]) -> int:
     current_depth = 0
     current_horizontal = 0
-    for command in data:
+    for command in inputs:
         match command[0]:
             case "down":
                 current_depth += command[1]
@@ -11,12 +14,31 @@ def calculate_route_and_return_product(data: List[(str, int)]) -> int:
                 current_horizontal += command[1]
     return current_depth * current_horizontal
 
+
+def calculate_route_with_aim_and_return_product(inputs: list[(str, int)]) -> int:
+    current_depth = 0
+    current_horizontal = 0
+    current_aim = 0
+    for command in inputs:
+        match command[0]:
+            case "down":
+                current_aim += command[1]
+            case "up":
+                current_aim -= command[1]
+            case "forward":
+                current_horizontal += command[1]
+                current_depth += command[1] * current_aim
+    return current_depth * current_horizontal
+
+
 if __name__ == '__main__':
-    with open('../Data/Day2Input.txt', 'r') as f:
+    with open(Path("../Data/Day2Input.txt"), 'r') as f:
         data = [x for x in f.read().splitlines()]
         route = []
         for line in data:
-            cmd = tuple(str(x[0]), int(x[1]) for x in line.split())
+            data = line.split()
+            data = [data[0], int(data[1])]
+            cmd = tuple(data)
             route.append(cmd)
-
     print(calculate_route_and_return_product(route))
+    print(calculate_route_with_aim_and_return_product(route))
