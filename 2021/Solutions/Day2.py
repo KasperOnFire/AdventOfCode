baa7f417dd47c67eafd4df933a36f52ed2f01c0e
Kasper RB <19799989+KasperOnFire@users.noreply.github.com>
2021-12-02 11:38:55 +0100

    fix glaring mistakes

--- 2021/Solutions/Day2.py
+++ 2021/Solutions/Day2.py
@@ -2,13 +2,13 @@ def calculate_route_and_return_product(data: List[(str, int)]) -> int:
     current_depth = 0
     current_horizontal = 0
     for command in data:
-        match data[0]:
+        match command[0]:
             case "down":
-                current_depth += data[1]
+                current_depth += command[1]
             case "up":
-                current_depth -= data[1]
+                current_depth -= command[1]
             case "forward":
-                current_horizontal += data[1]
+                current_horizontal += command[1]
     return current_depth * current_horizontal
 
 if __name__ == '__main__':
