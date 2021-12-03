0313f79f59f782ba823a2e74c01d88c723e2bf80
Kasper RB <19799989+KasperOnFire@users.noreply.github.com>
2021-12-03 10:48:22 +0100

    Update Day3.py

--- 2021/Solutions/Day3.py
+++ 2021/Solutions/Day3.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def calculate_gamma_and_epsilon(inputs: list[str]) -> int:
     threshold = len(inputs)/2
     counts = [0] * len(inputs[0])
@@ -14,22 +15,33 @@ def calculate_gamma_and_epsilon(inputs: list[str]) -> int:
 
     for i in counts:
         if i > threshold:
-            gamma_binary.append("1")
-            epsilon_binary.append("0")
+            gamma_binary += "1"
+            epsilon_binary += "0"
         elif i < threshold:
-            gamma_binary.append("0")
-            epsilon_binary.append("1")
+            gamma_binary += "0"
+            epsilon_binary += "1"
     
     gamma_decimal = int(gamma_binary, 2)
     epsilon_decimal = int(epsilon_binary, 2)
     return gamma_decimal * epsilon_decimal
 
+
+def verify_life_support_rating(inputs: list[str]) -> int:
+    oxygen_generator_rating = 0
+    co2_scrupper_rating = 0
+    counts = [0] * len(inputs[0])
+    for bit in inputs:
+        for pos, i in enumerate(bit):
+
+
             
-
-
 if __name__ == '__main__':
     with open(Path("../Data/Day3Input.txt"), 'r') as f:
         data = [x for x in f.read().splitlines()]
         test = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
-        print(calculate_gamma_and_epsilon(test))
 
+        print(calculate_gamma_and_epsilon(test))
+        print(calculate_gamma_and_epsilon(data))
+
+        print(verify_life_support_rating(test))
+        print(verify_life_support_rating(data))
