5fd953505fe845d33837dc68f1301de4050eae99
Kasper RB <19799989+KasperOnFire@users.noreply.github.com>
2021-12-08 11:09:47 +0100

    day08 part 1

--- 2021/Solutions/Day08.py
+++ 2021/Solutions/Day08.py
@@ -1,9 +1,25 @@
-import statistics as stats
 from pathlib import Path
 
 
 def part_1(circuit_input: list[tuple(str, str)]) -> int:
-    pass
+    number_freq = {}
+    for i in range(10):
+        number_freq[i] = 0
+    for input, output in circuit_input:
+        output_nums = output.split(" ")
+        for num in output_nums:
+             match len(num):
+                case 2:
+                    number_freq[1] += 1
+                case 3:
+                    number_freq[3] += 1
+                case 4:
+                    number_freq[4] += 1
+                case 7:
+                    number_freq[8] += 1
+                case _:
+                    pass
+    return sum(number_freq.values())
 
 
 def part_2(circuit_input: list[tuple(str, str)]) -> int:
